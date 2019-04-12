f151fee363be1ddaf093540943360e62a32a96a3
xxCUBSxx <xxCUBSxx@hotmail.com>
2019-04-11 23:24:34 -0500

    Update JSONDatabase.cs

--- RevivingSun/DataStore/JSONDatabase.cs
+++ RevivingSun/DataStore/JSONDatabase.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace BreakinIn.DataStore
+namespace RevivingSun.DataStore
 {
     public class JSONDatabase : IDatabase
     {
